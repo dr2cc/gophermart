f61b30690b02bf66b2e68f0200efb40e2fdbe0f5
Constantine <tmail99@gmail.com>
2026-02-21 16:38:48 +0300

    minor sql

--- db/migrations/20260215150000_create_orders_tables.sql
+++ db/migrations/20260215150000_create_orders_tables.sql
@@ -2,25 +2,35 @@
 -- +goose StatementBegin
 -- 1. Таблица заказов
 -- ❌ПРОВЕРИТЬ КАК В ОРИГИНАЛАХ- ♊ и уже готовом
+-- 1. Создаем пользовательский тип данных
+CREATE TYPE task_status AS ENUM (
+    'NEW', 
+    'PROCESSING', 
+    'INVALID', 
+    'PROCESSED'
+);
+-- 15:19
 CREATE TABLE orders (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, -- уникальный id записи
     -- number
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    order_id UUID, -- UUID это специальный тип для заказов(!) ключ для поиска? вместо number(?), с 12:23. Разобраться!
     user_id BIGINT NOT NULL REFERENCES users (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    order_id UUID, -- новый, с 9:26. Разобраться!
-    status VARCHAR(50) NOT NULL CHECK (
-        status IN (
-            'NEW',
-            'PROCESSING',
-            'INVALID',
-            'PROCESSED'
-        )
-    ),
+    -- TODO: status  ENUM для status - VARCHAR нет!
+    status task_status DEFAULT 'NEW', -- Можно задать значение по умолчанию
+    -- status VARCHAR(50) NOT NULL CHECK (
+    --     status IN (
+    --         'NEW',
+    --         'PROCESSING',
+    --         'INVALID',
+    --         'PROCESSED'
+    --     )
+    -- ),
     accrual NUMERIC(10, 2) DEFAULT 0, -- видимо сюда записывает свои данные accrual
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(), -- ex. uploaded_at 
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     attempts SMALLINT DEFAULT 0
 );
--- TODO: ENUM для status - VARCHAR нет!
+
 CREATE INDEX idx_orders_user_id ON orders (user_id);
 
 -- 2. Таблица баланса (связь 1-to-1 с пользователем)
