0b053a231ff901266ed09a9c197d12ae4e49e85a
Constantine <tmail99@gmail.com>
2026-03-09 16:14:26 +0300

    minor

--- go.mod
+++ go.mod
@@ -15,9 +15,12 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
--- internal/handler/auth_test.go
+++ internal/handler/auth_test.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"bytes"
+	"errors"
 	"gophermart/internal/models"
 	"gophermart/internal/service"
 	mock_service "gophermart/internal/service/mocks"
@@ -9,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/assert/v2"
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
 
@@ -33,7 +34,9 @@ func TestHandler_signUp(t *testing.T) {
 				Login:    "username",
 				Password: "qwerty",
 			},
+			// ожидания от мока
 			mockBehavior: func(s *mock_service.MockAuthorization, user models.User) {
+				// Тело функции.
 				// У объекта мока s вызываем метод EXPECT (s.EXPECT()) = создаем поведение для s
 				// Через . указываем, что ожидаем получить вызов CreateUser с параметром (user models.User)
 				// Через следующую . пишем ожидание того, что вызов CreateUser вернет ID 1 и nil (ошибка)
@@ -42,33 +45,63 @@ func TestHandler_signUp(t *testing.T) {
 			expectedStatusCode:   200,
 			expectedResponseBody: `{"id":1}`,
 		},
+		{
+			name:      "Empty fields",
+			inputBody: `{"password": "qwerty"}`,
+			mockBehavior: func(s *mock_service.MockAuthorization, user models.User) {
+				// тело функции не указываем, т.к вызова метода сервиса не будет- нет данных
+			},
+			expectedStatusCode:   400,
+			expectedResponseBody: `{"message":"invalid input body"}`,
+		},
+		{
+			name:      "Service failure",
+			inputBody: `{"login": "username", "password": "qwerty"}`,
+			inputUser: models.User{
+				Login:    "username",
+				Password: "qwerty",
+			},
+			// ожидания от мока
+			mockBehavior: func(s *mock_service.MockAuthorization, user models.User) {
+				// Тело функции.
+				// ожидание того, что вызов CreateUser вернет ID 1 и errors.New("service failure")
+				s.EXPECT().CreateUser(user).Return(1, errors.New("service failure"))
+			},
+			expectedStatusCode:   500,
+			expectedResponseBody: `{"message":"service failure"}`,
+		},
 	}
 
 	for _, test := range testTable {
-		// По сути повторяем здесь упрощенную только под задачи теста "точку сборки" Run.
+		// По сути повторяем здесь упрощенную (только под задачи теста) "точку сборки" Run.
 		t.Run(test.name, func(t *testing.T) {
-
 			// 1. Сборка зависимостей: ctrl--> services--> handler
 			// Инициализация моков.
 			// Создаем "ложный" контроллер (требование библиотеки- создавать контроллер
 			// и "финишировать" его по выполнению теста)
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
-			// Создаем "ложный" сервис
+			// Создаем "ложный" сервис, который притворяется реальной бизнес-логикой (интерфейсом Authorization).
 			auth := mock_service.NewMockAuthorization(ctrl)
 			// Передавая параметры (auth, test.inputUser) мы указываем, что
 			// ожидаем получить вызов метода сервиса, а в качестве аргумента inputUser models.User
 			test.mockBehavior(auth, test.inputUser)
-			//Создаем объект сервисов, но передадим аргументом для интерфейса авторизации наш "ложный" auth.
+			// Создаем объект сервисов, но передадим аргументом для интерфейса авторизации наш "ложный" auth.
+			// service.Service думает, что работает с настоящей базой или API, хотя на самом деле он работает с нашим «контролируемым» моком.
 			services := &service.Service{Authorization: auth}
-			// Инициализируем хендлер
+			// Инициализируем хендлер.
+			// Структура controller получает объект services, внутри которого уже есть наш мок.
+			// Хендлер не знает, как регистрировать пользователя, он лишь делегирует это сервису.
+			// Теперь при вызове handler.signUp внутри сработает цепочка, ведущая к моку.
 			handler := controller{services}
 
 			// 2. Init Endpoint
 			r := gin.New()
+			// Регистрируем конкретную функцию контроллера (тестируемый метод хендлера signUp) на маршрут /register
+			// Gin теперь знает: «Если придет POST-запрос на этот адрес, нужно запустить именно этот код».
 			r.POST("/register", handler.signUp)
 
-			// Create Request
+			// Создадим запрос к тестируемуму методу
 			w := httptest.NewRecorder()
 			req := httptest.NewRequest("POST", "/register",
 				//bytes.NewBufferString(test.inputBody) создает тело нашего запроса
@@ -76,11 +109,21 @@ func TestHandler_signUp(t *testing.T) {
 				bytes.NewBufferString(test.inputBody))
 
 			// Make Request
+			// Вызываем метод ServeHTTP у объекта роутера
+			// Отдаем роутеру «виртуальный» запрос (req) и «записывающее устройство» (w).
+			// Роутер прогоняет запрос через свои механизмы, вызывает ваш хендлер, тот вызывает сервис (мок!),
+			// получает ответ и записывает результат в w.
 			r.ServeHTTP(w, req)
 
-			// Assert
-			assert.Equal(t, w.Code, test.expectedStatusCode)
-			assert.Equal(t, w.Body.String(), test.expectedResponseBody)
+			// Проверка (Assert, точнее "проверка утверждения").
+			// 1. Проверяем код состояния (Status Code)
+			// Мы ожидаем, например, 200 OK или 201 Created, если тест успешный.
+			// test.expectedStatusCode уже заранее определен в структуре теста.
+			assert.Equal(t, test.expectedStatusCode, w.Code)
+			// 2. Проверяем тело ответа (Response Body)
+			// w.Body.String() возвращает то, что хендлер записал в ответ.
+			// Мы сравниваем это с ожидаемой строкой из нашего тест-кейса.
+			assert.Equal(t, test.expectedResponseBody, w.Body.String())
 		})
 	}
 }
